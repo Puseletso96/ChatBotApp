4fa2d7a4368deb2cf73100a756c8a425dd645670
Puseletso96 <ST10498947@rcconnect.edu.za>
2026-08-24 18:03:45 +0200

    intial commits

---
+++ ChatBotApp/AudioPlayer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatBotApp
+{
+    internal class AudioPlayer
+    {
+    }
+}
--- ChatBotApp/Program.cs
+++ ChatBotApp/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+
+namespace CyberChatBot;
+
+internal class Program
+{
+    static void Main()
+    {
+        audioPlayer audio = new audioPlayer();
+        
+
+    }
+}
