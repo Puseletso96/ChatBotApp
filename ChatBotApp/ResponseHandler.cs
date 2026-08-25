efa566845e7a0c1c012c12310676aeec89595bb2
Puseletso96 <ST10498947@rcconnect.edu.za>
2026-08-25 15:21:23 +0200

    Added conted in the response handler

--- ChatBotApp/ResponseHandler.cs
+++ ChatBotApp/ResponseHandler.cs
@@ -7,5 +7,30 @@ namespace ChatBotApp
     internal class ResponseHandler
     {
         public string Name { get; set; }
+        public string GetResponse(string input)
+        {
+            if (input.Contains("how areyou") || input.Contains("how are u"))
+
+            {
+                return $"I'm doing well thanks for asking!";
+            }
+            else if (input.Contains("purpose"))
+            {
+                return $" I'm your cyber security bot Assistant{Name}! I'm here to educate you about online safety, phishing scams, password security, and safe browsing habits";
+
+            }
+            else if (input.Contains("what can i ask") || input.Contains("what can i ask you"))
+            {
+                return $" Great question, {Name}! You can ask me about online safety, phishing scams, password security, and safe browsing habits. I'm here to help you stay safe online!";
+            }
+            else if (input.Contains("help"))
+            {
+                return $" Here's what i can help you with,{Name}!: \n Password safety tips \n How to spot phishing emails\n safe browsing practice\n Recognising social engineering\n\nType 'exit' to end our chat";
+        }
+            else
+            {
+                return $"I'm sorry, {Name}, I didn't understand that. Can you please rephrase your question or ask about online safety, phishing scams, password security, or safe browsing habits?";
+            }
+        }
     }
 }
